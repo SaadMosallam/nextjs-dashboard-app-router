1df9479bac8487b03451af5233cc42095b3af398
Saad <saad.mosallam@4sale.tech>
2025-01-29 16:36:23 +0200

    add fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'], // Specify weights to use
+  subsets: ['latin'] // Specify subsets if needed
+});
